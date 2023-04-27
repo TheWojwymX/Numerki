449a3e67ace4d20e7b15e21ed185ee0f7f856efa
TheWojwymX <thewojwymx@o2.pl>
2023-04-27 17:04:53 +0200

    Minor fix

--- zad3/menus.py
+++ zad3/menus.py
@@ -8,6 +8,6 @@ def function_choice() -> None:
     print("4. f(x) = sin(x)")
     print("5. f(x) = cos(-0.5x^3 + 2x^2 + x + 3)")
     print("6. f(x) = |cos(x)|")
-    print("7. f(x) = |sin(-x^3 + 2x^2 - x - 3)|")
+    print("7. f(x) = |sin(-0.5x^3 + 2x^2 - x - 3)|")
 
 
